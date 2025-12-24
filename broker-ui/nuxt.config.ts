827f9088e08c9733a2723cc1e24b841dbaf62699
m.habibian <mojtabaa.hn@gmail.com>
2025-12-24 19:18:55 +0330

    feat(frontend): adds shadcn-vue

--- broker-ui/nuxt.config.ts
+++ broker-ui/nuxt.config.ts
@@ -2,13 +2,15 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
-    css: ['~/assets/css/tailwind.css'],
-    vite: {
-        plugins: [
-            tailwindcss(),
-        ],
-    },
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true},
+  css: ['~/assets/css/tailwind.css'],
 
+  vite: {
+      plugins: [
+          tailwindcss(),
+      ],
+  },
+
+  modules: ['shadcn-nuxt'],
 })
